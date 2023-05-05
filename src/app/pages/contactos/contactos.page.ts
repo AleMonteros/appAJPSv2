cf7af17387e0a779e642e6bc4e7cd168a82572b1
AleMonteros <alejandromonteros236@gmail.com>
2023-05-05 09:57:42 -0300

    modified:   src/app/pages/contactos/contactos.page.html 	modified:   src/app/pages/contactos/contactos.page.ts

--- src/app/pages/contactos/contactos.page.ts
+++ src/app/pages/contactos/contactos.page.ts
@@ -44,4 +44,30 @@ export class ContactosPage implements OnInit {
   goToElSauce(){
     window.open("https://www.elsaucesalta.com.ar/es/", "-blank");
   }
+
+  // Navgacion de los Medios Contactos
+
+  contWhatsApp(){
+    window.open("https://api.whatsapp.com/send?phone=543876856999&text=Hola%2C%20he%20entrado%20a%20%20su%20web%20y%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios%20", "-blank");
+  }
+
+  contFacebook(){
+    window.open("https://www.facebook.com/AsocdeJubSalta","-blank");
+  }
+
+  contInstagram(){
+    window.open("https://instagram.com/asocdejubsalta?igshid=YmMyMTA2M2Y=", "-blank");
+  }
+
+  contUbicacion(){
+    window.open("https://goo.gl/maps/ThFoZtNa53JedA9N8", "-blank");
+  }
+
+  contEmail(){
+    window.open("mailto:info@asocdejubsalta.com.ar", "-blank");
+  }
+
+  contTelefono(){
+    window.open("tel:3876856999", "-blank");
+  }
 }
